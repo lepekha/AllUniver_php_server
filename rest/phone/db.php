7212df330033dd0889baf6ce5add8ff237fb98bd
Ruslan Lepekha <ruslan.lepekha@gmail.com>
2017-10-15 22:58:15 +0300

    create folder phone_2

--- rest/phone/db.php
+++ rest/phone/db.php
@@ -1,7 +1,7 @@
 <?php   
 
-$user = "id3267118_rootruslan";
-$pass = "rlo1992";
+$user = "";
+$pass = "";
 $pdo = new PDO('mysql:host=localhost;dbname=id3267118_alluniver', $user, $pass,array(
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
 )
